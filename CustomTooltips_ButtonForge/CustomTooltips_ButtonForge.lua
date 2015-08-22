bd26036406f0c45c6701663be6cfb8f6ded703c3
Choonster <Choonster.2010@gmail.com>
2015-08-23 04:38:35 +1000

    Add ButtonForge module

---
+++ CustomTooltips_ButtonForge/CustomTooltips_ButtonForge.lua
@@ -0,0 +1,9 @@
+local API = ButtonForge_API1
+
+-- We need to hook this method directly rather than using the BUTTON_ALLOCATED callback because
+-- the method is set as Widget.UpdateTooltip, which GameTooltip will call from OnUpdate while owned by the widget.
+-- This means that simply setting an OnEnter hook won't work, as the custom tooltip will be replaced almost instantly.
+hooksecurefunc(BFButton, "UpdateTooltipMacro", function(self)
+	self = self.ParentButton or self
+	CustomTooltips_DisplayTooltip(self.Widget, self.MacroBody)
+end)
