f11b3992b0e5258dce2a83e39d808187625dc931
Choonster TheMage <Choonster.2010@gmail.com>
2024-04-28 21:50:37 +1000

    Update publish.ps1 for new PublishAddon version

--- publish.ps1
+++ publish.ps1
@@ -2,11 +2,10 @@
 .SYNOPSIS
     Packages the AddOn with the BigWigs Packager and copies it to the WoW game directories.
     
-    Requires https://github.com/BigWigsMods/packager to be installed in ~\source\repos\packager and https://github.com/Tuller/PublishAddon to be installed in ~\source\repos\PublishAddon.
+    Requires https://github.com/Tuller/PublishAddon to be installed in ~\source\repos\PublishAddon.
 #>
 
 $global:WOW_HOME = 'D:\World of Warcraft'
-$global:WOW_PACKAGER = wsl wslpath $(Resolve-Path '~\source\repos\packager\release.sh').Path.Replace('\', '\\')
 
 Import-Module '~\source\repos\PublishAddon\wow.psm1'
 
