076c7f387b720cfd756dcbbab45f7386835408bb
Choonster TheMage <Choonster.2010@gmail.com>
2024-08-25 18:56:53 +1000

    Add Lua Language Server type annotations to core.lua - Format code

--- core.lua
+++ core.lua
@@ -41,19 +41,24 @@ ADDITIONAL NOTES
    specify line breaks. (See the first example for more details.)
 ]===]
 
+--- @type table<string, Tooltip>
 local TOOLTIPS = {
--- Put your tooltip definitions below this line
+	-- Put your tooltip definitions below this line
 
 	Example = {
 		heading = "Example Tooltip Heading",
-		body = {{"This is an example tooltip. The text will be in red.", 1.0, 0.0, 0.0},
-		        {"This text is on a separate line. The text will have the default yellow text colour."},
-		        {"This is the third line of the tooltip. It will be blue.", 0.0, 0.0, 1.0}}
+		body = {
+			{ "This is an example tooltip. The text will be in red.",                               1.0, 0.0, 0.0 },
+			{ "This text is on a separate line. The text will have the default yellow text colour." },
+			{ "This is the third line of the tooltip. It will be blue.",                            0.0, 0.0, 1.0 }
+		}
 	},
 
 	ExampleTwo = {
 		heading = "Second Example Heading",
-		body = {{"This is a second example tooltip; it doesn't have any linebreaks in it. It uses the default yellow colour."}}
+		body = {
+			{ "This is a second example tooltip; it doesn't have any linebreaks in it. It uses the default yellow colour." }
+		}
 	},
 
 	-- This is a test example, to ensure that the syntax checker is working properly
@@ -64,22 +69,39 @@ local TOOLTIPS = {
 		-- this is actually 2 errors-in-one
 		-- first of all, the red value is specified, but neither the green nor the blue values are specified, so thats one error
 		-- the second error in this line is that the red value is not between 0 and 1 inclusive
-		body = {{"This is an erroneous tooltip. If used, it should display the syntax error tooltip.", 2.0}}
+		body = {
+			{ "This is an erroneous tooltip. If used, it should display the syntax error tooltip.", 2.0 }
+		}
 	},
 
--- Put your tooltip definitions above this line
+	-- Put your tooltip definitions above this line
 }
 
 -------------------
 -- END OF CONFIG --
 -------------------
 
--- Do not change this
+-- Do not change anything below this line
+
+--- @class TooltipLine
+--- @field [1] string Tooltip text
+--- @field [2] number? Red
+--- @field [3] number? Green
+--- @field [4] number? Blue
+
+--- @alias TooltipBody TooltipLine[]
+
+--- @class Tooltip
+--- @field heading string
+--- @field body TooltipBody
+
 local ERROR_SYNTAX_TOOLTIP = {
 	heading = "Tooltip Syntax Error",
-	body = {"Syntax error in tooltip definition in core.lua:",
-	        "Error: %s in tooltip %s",
-	        "%s"}
+	body = {
+		"Syntax error in tooltip definition in core.lua:",
+		"Error: %s in tooltip %s",
+		"%s"
+	}
 }
 
 local ERROR_NOT_FOUND_TOOLTIP = {
@@ -87,22 +109,38 @@ local ERROR_NOT_FOUND_TOOLTIP = {
 	body = "Error: Custom tooltip definition not found with name \"%s\"."
 }
 
+---@param errorMessage string
+---@param tooltipName  string
+---@param detailedMessage string
+---@return string heading
+---@return TooltipBody body
 local function GetSyntaxError(errorMessage, tooltipName, detailedMessage)
 	local heading = ERROR_SYNTAX_TOOLTIP.heading
-	local body = {{ERROR_SYNTAX_TOOLTIP.body[1], 1.0, 0.0, 0.0},
-	              {ERROR_SYNTAX_TOOLTIP.body[2]:format(errorMessage, tooltipName), 1.0, 0.0, 0.0},
-	              {ERROR_SYNTAX_TOOLTIP.body[3]:format(detailedMessage), 1.0, 0.0, 0.0}}
+	local body = {
+		{ ERROR_SYNTAX_TOOLTIP.body[1],                                   1.0, 0.0, 0.0 },
+		{ ERROR_SYNTAX_TOOLTIP.body[2]:format(errorMessage, tooltipName), 1.0, 0.0, 0.0 },
+		{ ERROR_SYNTAX_TOOLTIP.body[3]:format(detailedMessage),           1.0, 0.0, 0.0 }
+	}
 
 	return heading, body
 end
 
+---@param name string
+---@return string heading
+---@return TooltipBody body
 local function GetNotFoundError(name)
 	local heading = ERROR_NOT_FOUND_TOOLTIP.heading
-	local body = {{ERROR_NOT_FOUND_TOOLTIP.body:format(name), 1.0, 0.0, 0.0}}
+	local body = {
+		{ ERROR_NOT_FOUND_TOOLTIP.body:format(name), 1.0, 0.0, 0.0 }
+	}
 
 	return heading, body
 end
 
+---@param name string
+---@param data Tooltip
+---@return string name
+---@return Tooltip data
 local function CheckSyntax(name, data)
 	local heading, body = data.heading, data.body
 	local hasError = false
@@ -128,39 +166,75 @@ local function CheckSyntax(name, data)
 	end
 
 	if not hasError then
-		for i=1, #body do
+		for i = 1, #body do
 			local line = body[i]
 			if not hasError and type(line) ~= "table" then
-				heading, body = GetSyntaxError("body line is not a table", name, ("Line %d of the tooltip's body must be a table."):format(i))
+				heading, body = GetSyntaxError(
+					"body line is not a table",
+					name,
+					("Line %d of the tooltip's body must be a table."):format(i)
+				)
+
 				hasError = true
 			end
 
 			local text = line[1]
 			if not hasError and not text then
-				heading, body = GetSyntaxError("line text not found", name, ("Line %d of the tooltip's body is missing text."):format(i))
+				heading, body = GetSyntaxError(
+					"line text not found",
+					name,
+					("Line %d of the tooltip's body is missing text."):format(i)
+				)
+
 				hasError = true
 			end
 
 			local red, green, blue = line[2], line[3], line[4]
 			if not hasError and red and (not green or not blue) then
-				heading, body = GetSyntaxError("colour value(s) missing", name, ("Line %d of the tooltip's body must either specify all 3 colour components or none."):format(i))
+				heading, body = GetSyntaxError(
+					"colour value(s) missing",
+					name,
+					("Line %d of the tooltip's body must either specify all 3 colour components or none."):format(i)
+				)
+
 				hasError = true
 			end
 
 			-- We know red, green, and blue are all defined
 			if not hasError and red then
 				if type(red) ~= "number" or red < 0.0 or red > 1.0 then
-					heading, body = GetSyntaxError("invalid red value", name, ("Line %d of the tooltip's body: red value must be a number between 0.0 and 1.0 inclusive."):format(i))
+					heading, body = GetSyntaxError(
+						"invalid red value",
+						name,
+						(
+							"Line %d of the tooltip's body: red value must be a number between 0.0 and 1.0 inclusive."
+						):format(i)
+					)
+
 					hasError = true
 				end
 
 				if type(green) ~= "number" or green < 0.0 or green > 1.0 then
-					heading, body = GetSyntaxError("invalid green value", name, ("Line %d of the tooltip's body: green value must be a number between 0.0 and 1.0 inclusive."):format(i))
+					heading, body = GetSyntaxError(
+						"invalid green value",
+						name,
+						(
+							"Line %d of the tooltip's body: green value must be a number between 0.0 and 1.0 inclusive."
+						):format(i)
+					)
+
 					hasError = true
 				end
 
 				if type(blue) ~= "number" or blue < 0.0 or blue > 1.0 then
-					heading, body = GetSyntaxError("invalid blue value", name, ("Line %d of the tooltip's body: blue value must be a number between 0.0 and 1.0 inclusive."):format(i))
+					heading, body = GetSyntaxError(
+						"invalid blue value",
+						name,
+						(
+							"Line %d of the tooltip's body: blue value must be a number between 0.0 and 1.0 inclusive."
+						):format(i)
+					)
+
 					hasError = true
 				end
 			end
@@ -172,11 +246,12 @@ local function CheckSyntax(name, data)
 	return name, data
 end
 
+--- @type table<string, Tooltip>?
 local oldTooltips = TOOLTIPS
 TOOLTIPS = {} -- Make a new table to store the normalised tooltips in (we can't add new keys when iterating with pairs)
 
 -- Process the tooltip definitions to normalise them.
-for name, data in pairs(oldTooltips) do
+for name, data in pairs(assert(oldTooltips)) do
 	name, data = CheckSyntax(name, data)
 
 	-- Convert the keys to uppercase
@@ -191,10 +266,10 @@ oldTooltips = nil -- Allow the old table to be garbage collected.
 CustomTooltips = {}
 
 --- Gets the heading and body of a tooltip defined in the TOOLTIPS table.
--- @param name The name of the tooltip
--- @return true if the tooltip was found; false otherwise
--- @return The heading of the tooltip
--- @return The body of the tooltip (a table of line definitions)
+--- @param name string The name of the tooltip
+--- @return boolean success true if the tooltip was found; false otherwise
+--- @return string heading The heading of the tooltip
+--- @return Tooltip body The body of the tooltip (a table of line definitions)
 function CustomTooltips.GetNamedTooltip(name)
 	local tooltipData = TOOLTIPS[name:upper()]
 	if tooltipData then
@@ -205,33 +280,37 @@ function CustomTooltips.GetNamedTooltip(name)
 end
 
 --- Gets the heading and body of a tooltip defined inline.
--- @param definition The text following the #tooltipdesc metacommand
--- @return true if a valid tooltip was found; false otherwise
--- @return The heading of the tooltip
--- @return The body of the tooltip (a table of line definitions)
+--- @param definition string The text following the #tooltipdesc metacommand
+--- @return boolean success  true if a valid tooltip was found; false otherwise
+--- @return string heading The heading of the tooltip
+--- @return TooltipBody body The body of the tooltip (a table of line definitions)
 function CustomTooltips.GetInlineTooltip(definition)
 	-- Find the heading and body from the inline tooltip definition (use a non-greedy approach to the separator - heading ends at
 	-- the first ^, and all subsequent ^ will be part of the body)
 	local heading, body = definition:match("([^\n\\^][^\n\\^]-)^([^\n]+)")
 	if heading and body then
 		local text = body:gsub("\\n", "\n")
-		body = {{ text, nil, nil, nil }}
+		body = { { text, nil, nil, nil } }
 		return true, heading, body
 	else
-		return false, GetSyntaxError("Bad inline tooltip definition format", "<inline>", "Macro inline tooltip must follow this format: #tooltipdesc heading^body")
+		return false,
+			GetSyntaxError(
+				"Bad inline tooltip definition format", "<inline>",
+				"Macro inline tooltip must follow this format: #tooltipdesc heading^body"
+			)
 	end
 end
 
 --- Populates a GameTooltip with the specified tooltip text.
--- @param tooltipFrame The GameTooltip to populate
--- @param heading The heading of the tooltip
--- @param body The body of the tooltip (a table of line definitions)
+--- @param tooltipFrame GameTooltip The GameTooltip to populate
+--- @param heading string The heading of the tooltip
+--- @param body TooltipBody The body of the tooltip (a table of line definitions)
 function CustomTooltips.SetTooltipText(tooltipFrame, heading, body)
 	tooltipFrame:ClearLines()
 
-	tooltipFrame:AddLine(heading, 1,1,1) -- Use white text, don't wrap the text
+	tooltipFrame:AddLine(heading, 1, 1, 1) -- Use white text, don't wrap the text
 
-	for i=1, #body do
+	for i = 1, #body do
 		-- If the colours were not specified, these values are nil, which tells the GameTooltip to use the default yellow
 		local text, red, green, blue = unpack(body[i], 1, 4)
 		tooltipFrame:AddLine(text, red, green, blue, true)
@@ -241,8 +320,8 @@ function CustomTooltips.SetTooltipText(tooltipFrame, heading, body)
 end
 
 --- Displays a custom tooltip anchored to the specified button based on the specified macro text.
--- @param button The button to anchor the tooltip to, or nil if the tooltip shouldn't be anchored to a button
--- @param macroText The macro text
+--- @param button Button The button to anchor the tooltip to, or nil if the tooltip shouldn't be anchored to a button
+--- @param macroText string The macro text
 function CustomTooltips.DisplayTooltipForMacroText(button, macroText)
 	local _, heading, body
 
